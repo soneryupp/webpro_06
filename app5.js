7a47409d20b26e1b0bcb54909c40469ea9c3dda3
Ryuya Sonehara <iiyama.jiji@gmail.com>
2025-11-14 11:45:31 +0900

    hello

--- app5.js
+++ app5.js
@@ -99,4 +99,24 @@ app.get("/keiyo_add", (req, res) => {
   res.render('db1', { data: station });
 });
 
+let station2 = [
+  { id:1, code:"JE01", name:"東京駅", change:"総武本線，中央線，etc", passengers:403831, distance:0 },
+  { id:2, code:"JE02", name:"八丁堀駅", change:"日比谷線", passengers:31071, distance:1.2 },
+  { id:3, code:"JE05", name:"新木場駅", change:"有楽町線，りんかい線", passengers:67206, distance:7.4 },
+  { id:4, code:"JE07", name:"舞浜駅", change:"舞浜リゾートライン", passengers:76156,distance:12.7 },
+  { id:5, code:"JE12", name:"新習志野駅", change:"", passengers:11655, distance:28.3 },
+  { id:6, code:"JE17", name:"千葉みなと駅", change:"千葉都市モノレール", passengers:16602, distance:39.0 },
+  { id:7, code:"JE18", name:"蘇我駅", change:"内房線，外房線", passengers:31328, distance:43.0 },
+];
+
+app.get("/keiyo2", (req, res) =>{
+  res.render('keiyo2', {data: station2});
+});
+
+app.get("/keiyo2/:number", (req, res) =>{
+  const number = req.params.number;
+  const detail = station2[number];
+  res.render('keiyo2_detail',{data: detail});
+})
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
